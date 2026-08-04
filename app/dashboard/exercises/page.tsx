b2945d6043515dc8432000b6da2a38029b076437
Anmol Bhalla <abhalla@karorakr8ztech.onmicrosoft.com>
2026-08-04 11:46:56 +0530

    adding static excersise page

---
+++ app/dashboard/exercises/page.tsx
@@ -0,0 +1,10 @@
+export default function ExercisesPage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Exercises</h1>
+      <p className="text-gray-500">
+        This page is under development.
+      </p>
+    </div>
+  );
+}
